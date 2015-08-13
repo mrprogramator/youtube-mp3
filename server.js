77d5ad554815f0558b2bf42781c1fcdb20512cb0
Luis Saucedo <luisaucedoquiroga@gmail.com>
2015-08-12 23:49:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var child_process = require('child_process');
+var sys = require('sys');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var util  = require('util');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '/') });
+});
+
+var server = app.listen(4040,function () {
+  console.log("listening");
+});
+
+app.post('/', function(req, res) {
+  var url = req.body.name;
+  console.log('uploading music');
+  
+  dl = child_process.spawn("youtube-dl", ['-o', url + ".%(ext)s", '--extract-audio','--audio-format', 'mp3','-c','ytsearch:' + url]);
+  
+  dl.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+
+  dl.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  dl.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+    var file = __dirname + '/' + url + '.mp3';
+    res.download(file);
+    res.on('finish', function () { 
+      console.log('F I N I S H E D');
+      fs.unlinkSync(file);
+    });
+  });
+});
+
+app.post('/video', function(req, res) {
+  var url = req.body.videoUrl;
+  console.log('uploading video');
+  
+  dl = child_process.spawn("youtube-dl", ['-o', url + ".%(ext)s",'-c','ytsearch:' + url]);
+  
+  dl.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+
+  dl.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  dl.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+    var file = __dirname + '/' + url + '.mp4' ;
+    res.download(file);
+    res.on('finish', function () { 
+      console.log('F I N I S H E D');
+      fs.unlinkSync(file);
+    });
+  });
+});
