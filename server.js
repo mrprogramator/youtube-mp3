a864718d37aefb4002e7d1f7e6bf8bac13b0c1ad
mrprogramator <luisaucedoquiroga@gmail.com>
2015-08-15 13:14:56 -0400

    Getting song name

--- server.js
+++ server.js
@@ -19,43 +19,60 @@ var server = app.listen(process.env.PORT || 8080,function () {
 
 app.post('/', function(req, res) {
   var url = req.body.name;
+  var name = '';
+  if (url.indexOf(''))
   console.log('uploading music: ' + url);
-  child_process.spawn("pwd",['-LP']);
-  dl = child_process.spawn("./youtube-dl", ['--ffmpeg-location','./ffmpeg','-o', url + ".%(ext)s", '--extract-audio','--audio-format','mp3','-c','ytsearch:' + url]);
+  fname = child_process.spawn("./youtube-dl", ['--get-filename','--ffmpeg-location','./ffmpeg','-o', "'%(title)s'.'%(ext)s'", '--extract-audio','--audio-format','mp3','-c','ytsearch:' + url]);
   
-  dl.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
+  fname.stdout.on('data', function (data) {
+    name = data;
   });
-
-  dl.stderr.on('data', function (data) {
+  
+  fname.stderr.on('data', function (data) {
     console.log('stderr: ' + data);
+    name = url;
   });
-
-  dl.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-    
-    glob(url + '.*', function(err, files) {
-      if (files.length == 0){
-        console.log('No se encuentra el archivo');
-        res.send('No se encuentra el archivo ' + url);
-        return;
-      }
-      
-      var file = files[0];
-      res.download(file);
-      res.on('finish', function () {
-        console.log('F I N I S H E D');
-        console.log('deleting ', file);
-        fs.unlinkSync(file, function (err) {
-          if (err){
-            res.send('Error al borrar archivo mp3 :' + err);
-            return;
-          }
-          console.log('successfully deleted ' + url + '.mp3');
+  
+  fname.on('finish', function (code) {
+    console.log('Finishing obtaining name with code ' + code);
+    dl = child_process.spawn("./youtube-dl", ['--ffmpeg-location','./ffmpeg','-o',"'%(title)s'.'%(ext)s'", '--extract-audio','--audio-format','mp3','-c','ytsearch:' + url]);
+  
+    dl.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+  
+    dl.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+  
+    dl.on('exit', function (code) {
+      console.log('child process exited with code ' + code);
+      var nameUncode = name.substring(0, name.indexOf('.'));
+      glob(nameUncode + '.*', function(err, files) {
+        if (files.length == 0){
+          console.log('No se encuentra el archivo');
+          res.send('No se encuentra el archivo ' + nameUncode);
+          return;
+        }
+        
+        var file = files[0];
+        res.download(file);
+        res.on('finish', function () {
+          console.log('F I N I S H E D');
+          console.log('deleting ', file);
+          fs.unlinkSync(file, function (err) {
+            if (err){
+              res.send('Error al borrar archivo mp3 :' + err);
+              return;
+            }
+            console.log('successfully deleted ' + nameUncode + '.mp3');
+          });
         });
       });
     });
   });
+  
+  
 });
 
 app.post('/video', function(req, res) {
