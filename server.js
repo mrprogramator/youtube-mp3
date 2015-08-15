47dffc7a767c289de27266f8f03c379c7d97329c
Luis Saucedo <luisaucedoquiroga@gmail.com>
2015-08-14 22:02:06 -0400

    adding ffmpeg

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var util  = require('util');
 var glob = require('glob');
-var ffmpeg = require('ffmpeg');
-var ffprobe = require('node-ffprobe');
-
-var Ffmpeg = require('fluent-ffmpeg');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
@@ -25,7 +21,7 @@ app.post('/', function(req, res) {
   var url = req.body.name;
   console.log('uploading music: ' + url);
   
-  dl = child_process.spawn("./youtube-dl", ['-o', url + ".%(ext)s", '--extract-audio','-c','ytsearch:' + url]);
+  dl = child_process.spawn("./youtube-dl", ['--ffmpeg-location','./ffmpeg','-o', url + ".%(ext)s", '--extract-audio','--audio-format','mp3','-c','ytsearch:' + url]);
   
   dl.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
@@ -46,57 +42,18 @@ app.post('/', function(req, res) {
       }
       
       var file = files[0];
-      
-      var command = new Ffmpeg({source: file})
-        .withAudioCodec('libmp3lame')
-        .toFormat('mp3')
-        .saveToFile(url + '.mp3');
-      command.on('start', function () {
-        console.log('Starting convertion to mp3');
-      });
-      
-      command.on('progress', function(progress) {
-        console.log('targetSize:',progress.targetSize);
-      });
-      
-      command.on('error', function(err) {
-        console.log('Cannot convert audio: ' + err.message);
-      });
-      
-      command.on('end', function() {
-        console.log('Processing finished successfully');
-        glob(url + '.mp3', function(err, mp3s) {
-          if (mp3s.length == 0){
-            res.send('Cannot find mp3 file of ' + url);
+      res.download(file);
+      res.on('finish', function () {
+        console.log('F I N I S H E D');
+        console.log('deleting ', file);
+        fs.unlinkSync(file, function (err) {
+          if (err){
+            res.send('Error al borrar archivo mp3 :' + err);
             return;
           }
-          
-          console.log('deleting ', file);
-          fs.unlinkSync(file, function (err) {
-            if (err){
-              res.send('Error al borrar archivo m4a :' + err);
-              return;
-            }
-            console.log('successfully deleted ' + url + '.m4a');
-          });
-          
-          var mp3 = mp3s[0];
-          res.download(mp3);
-          res.on('finish', function () {
-            console.log('F I N I S H E D');
-            console.log('deleting ', mp3);
-            fs.unlinkSync(mp3, function (err) {
-              if (err){
-                res.send('Error al borrar archivo mp3 :' + err);
-                return;
-              }
-              console.log('successfully deleted ' + url + '.mp3');
-            });
-          });
+          console.log('successfully deleted ' + url + '.mp3');
         });
       });
-      
-      
     });
   });
 });
