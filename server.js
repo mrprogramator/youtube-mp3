4f043826563b07ecf18076a6b11970e54d1bdb71
Luis Saucedo <luisaucedoquiroga@gmail.com>
2016-04-03 14:17:12 -0400

    Heroku timeout cannot be set

--- server.js
+++ server.js
@@ -16,143 +16,159 @@ app.use(bodyParser.text());
 app.use(express.static(__dirname + ''));
 
 var server = app.listen(process.env.PORT || 8080, function () {
-  console.log('listening on PORT:',server.address().port,'...');
+    console.log('listening on PORT:',server.address().port,'...');
 });
 
 server.timeout = 3000000000;
 
 app.get('/error', function (req, res) {
-  res.sendFile('error.html', { root: path.join(__dirname, '/') });
+    res.sendFile('error.html', { root: path.join(__dirname, '/') });
+});
+
+app.get('/upgrade', function (req, res) {
+    var process = child_process.spawn("./youtube-dl",['-U']);
+
+    var logProccess = "";
+    process.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+        logProccess += data.toString();
+        
+    });
+
+    process.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    
+    process.on('exit', function () {
+        res.send(logProccess);
+    })
 });
 
 app.post('/get-title', function(req, res) {
-  console.log(req.body);
-  var url = req.body.data;
-  var name = "EMPTY";
-  console.log('getting title...')
-  var process = child_process.spawn("./youtube-dl", ['--get-title','--default-search','ytsearch',url]);
-  
-  process.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-    name = data.toString();
-    res.send(name);
-  });
+    console.log(req.body);
+    var url = req.body.data;
+    var name = "EMPTY";
+    console.log('getting title...')
+    var process = child_process.spawn("./youtube-dl",
+     ['--get-title','--no-playlist','--default-search','ytsearch',url]);
 
-  process.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
+    process.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+        name = data.toString();
+        res.send(name);
+    });
+
+    process.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
 });
 
 app.post('/get-mp3', function(req, res) {
-  console.log(req.body);
-  
-  var url = req.body.url;
-  var name = req.body.name;
-  var folderName = req.body.folderName;
-  
-  
-  fs.mkdir(folderName, function () {
-    var process = child_process.spawn("./youtube-dl",['--ffmpeg-location','./ffmpeg','-o',folderName + "/%(title)s.%(ext)s",'--extract-audio','--audio-format','mp3','--default-search','ytsearch', url])
-    
-    process.stdout.on('data', function (data) {
-      console.log('stdout: ' + data);
-    });
+    console.log(req.body);
 
-    process.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-    
-    process.on('exit', function (code, data) {
-      console.log('process exit with code ' + code,data);
-      glob(folderName + "/*", function (err, files) {
-        if(files == 0) {
-          console.log('No se encuentra el archivo ' + name);
-          res.send('No se encuentra el archivo');
-        }
-        
-        var file = files[0];
-        
-        console.log('FILENAME', file);
-        res.send(file);
-        
-        res.on('finish', function () {
-          console.log('F I N I S H E D');
+    var url = req.body.url;
+    var name = req.body.name;
+    var folderName = req.body.folderName;
+
+    fs.mkdir(folderName, function () {
+        var process = child_process.spawn("./youtube-dl",
+            ['--ffmpeg-location','./ffmpeg','-o',
+            folderName + "/%(title)s.%(ext)s",
+            '--no-playlist','--extract-audio','--audio-format','mp3',
+            '--default-search','ytsearch', url])
+
+        process.stdout.on('data', function (data) {
+          console.log('stdout: ' + data);
         });
-      })
+
+        process.stderr.on('data', function (data) {
+          console.log('stderr: ' + data);
+        });
+
+        process.on('exit', function (code, data) {
+          console.log('process exit with code ' + code,data);
+          glob(folderName + "/*", function (err, files) {
+            if(files == 0) {
+              console.log('No se encuentra el archivo ' + name);
+              res.send('No se encuentra el archivo');
+            }
+            
+            var file = files[0];
+            
+            console.log('FILENAME', file);
+            res.send(file);
+            
+            res.on('finish', function () {
+              console.log('F I N I S H E D');
+            });
+          })
+        });
     });
-  });
-  
-  
-  
 })
 
 app.post('/get-mp4', function(req, res) {
-  console.log(req.body);
-  
-  var url = req.body.url;
-  var name = req.body.name;
-  var folderName = req.body.folderName;
-  
-  fs.mkdir(folderName, function () {
-    var process = child_process.spawn("./youtube-dl",['--ffmpeg-location','./ffmpeg','-o', folderName + "/%(title)s.%(ext)s",'--format','mp4','--default-search','ytsearch', url])
-    
-    process.stdout.on('data', function (data) {
-      console.log('stdout: ' + data);
-    });
+    console.log(req.body);
 
-    process.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-    
-    process.on('exit', function (code, data) {
-      console.log('process exit with code ' + code,data);
-      
-      glob(folderName + "/*", function (err, files) {
-        if(files == 0) {
-          console.log('No se encuentra el archivo ' + name);
-          res.send('No se encuentra el archivo');
-        }
-        
-        var file = files[0];
-        
-        console.log('FILENAME', file);
-        res.send(file);
-        
-        res.on('finish', function () {
-          console.log('F I N I S H E D');
+    var url = req.body.url;
+    var name = req.body.name;
+    var folderName = req.body.folderName;
+
+    fs.mkdir(folderName, function () {
+        var process = child_process.spawn("./youtube-dl",
+            ['--ffmpeg-location','./ffmpeg','-o',
+            folderName + "/%(title)s.%(ext)s",'--format',
+            'mp4','--no-playlist','--default-search','ytsearch', url])
+
+        process.stdout.on('data', function (data) {
+            console.log('stdout: ' + data);
         });
-      })
+
+        process.stderr.on('data', function (data) {
+            console.log('stderr: ' + data);
+        });
+
+        process.on('exit', function (code, data) {
+            console.log('process exit with code ' + code,data);
+
+            glob(folderName + "/*", function (err, files) {
+                if(files == 0) {
+                  console.log('No se encuentra el archivo ' + name);
+                  res.send('No se encuentra el archivo');
+                }
+
+                var file = files[0];
+
+                console.log('FILENAME', file);
+                res.send(file);
+
+                res.on('finish', function () {
+                  console.log('F I N I S H E D');
+                });
+            })
+        });
     });
-    
-  });
-  
-  
 })
 
-
 app.post('/download', function(req, res) {
-  console.log('Buscando el archivo ' + name);
-  
-  console.log(req.body);
+    console.log('Buscando el archivo ' + name);
 
-  var name = req.body.name;
-  
+    console.log(req.body);
+
+    var name = req.body.name;
+
     glob(name + "/*", function (err, files) {
-      if(files == 0) {
-        console.log('No se encuentra el archivo ' + name);
-        res.send(false);
-      }
-      
-      var file = files[0];
-      
-      console.log('FILENAME', file);
-      res.download(file);
-      
-      res.on('finish', function () {
-        console.log('F I N I S H E D');
-      });
+        if(files == 0) {
+            console.log('No se encuentra el archivo ' + name);
+            res.send(false);
+        }
+
+        var file = files[0];
+
+        console.log('FILENAME', file);
+        res.download(file);
+
+        res.on('finish', function () {
+            console.log('F I N I S H E D');
+        });
     })
-    
-  
-  
 })
