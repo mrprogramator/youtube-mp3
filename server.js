e2758c59962820b67a784b5352d10525215e0698
Luis Alberto Saucedo Quiroga <luisaucedoquiroga@gmail.com>
2020-04-12 06:15:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -274,7 +274,7 @@ app.get('/get-media', function (req, res){
             res.send(false);
         }
         else{
-            var file = files[files.length - 1];
+            var file = files[1];
             console.log('File to download:' + file);
             if(file){
                 var fileExt = file.split('.')[1];
